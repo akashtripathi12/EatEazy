28273b69dbe25a94779cb963bb1afc81ef1c7402
akashtripathi12 <140066601+akashtripathi12@users.noreply.github.com>
2024-08-29 18:54:46 +0530

    Update StoreContext.jsx

--- admin/src/context/StoreContext.jsx
+++ admin/src/context/StoreContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState } from "react";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const url = "http://localhost:8080";
+  const url = "https://eateazy-backend.onrender.com/";
   const [token, setToken] = useState("");
 
   useEffect(() => {
